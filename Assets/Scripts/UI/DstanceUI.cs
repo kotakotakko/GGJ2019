7b592d38598c6958ed8152887ce129f62c6bb131
門馬宏紀 <mommahiroki@yahoo.co.jp>
2019-01-26 02:25:58 +0900

    目標までの距離のUI

---
+++ Assets/Scripts/UI/DstanceUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DistanceUI : MonoBehaviour
+{
+
+    [SerializeField] private GameObject scoreText = null;
+
+    public void SetDistancText(float value)
+    {
+        scoreText.GetComponent<Text>().text = "目標までの距離:" + value.ToString("f1") + "m";
+    }
+}
