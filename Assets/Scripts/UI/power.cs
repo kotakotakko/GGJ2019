a9c3bf2f8e8f5603523a0b4df24bc9ed9206f07a
門馬宏紀 <mommahiroki@yahoo.co.jp>
2019-01-25 23:57:57 +0900

    PowerUI

---
+++ Assets/Scripts/UI/power.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; // ←※これを忘れずに入れる
+
+public class Power : MonoBehaviour
+{
+
+    Slider _slider;
+
+    public void SetMinPower(float value)
+    {
+        _slider.minValue = value;
+    }
+    public void SetMaxPower(float value)
+    {
+        _slider.maxValue = value;
+    }
+    public void SetValue(float value)
+    {
+        _slider.value = value;
+    }
+}
